a2a05f5d9eb6237b077c2a2845ac51ac595ec4a0
Ammar <akhtarammar29@gmail.com>
2025-03-11 21:53:26 +0700

    app2

--- app2.py
+++ app2.py
@@ -39,5 +39,5 @@ def kualitas_air():
 
     # return 'Data berhasil disimpan', 200
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
